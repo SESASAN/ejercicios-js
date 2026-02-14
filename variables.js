1d61232503f34df5039eec43161edc45cd5e99c2
SESASAN <sebastianjesus.perez@gmail.com>
2026-02-13 19:24:52 -0500

    feat: Add variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+var cantidad_de_manzanas = 7;
+let cantidad_de_peras = 10;
+const operacion = "Sumar";
+
+console.log(cantidad_de_manzanas);
+console.log(cantidad_de_peras);
+console.log(operacion);
+
+let esTodoMayuscula = undefined;
+
+console.log(esTodoMayuscula);
